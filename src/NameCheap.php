bf6e8471697fbabeb2a072d3660cb2b55485742b
reashetyrr <6582364+reashetyrr@users.noreply.github.com>
2020-11-06 14:10:33 +0100

    Update NameCheap.php

--- src/NameCheap.php
+++ src/NameCheap.php
@@ -7,7 +7,7 @@ use reashetyr\NameCheap\routes;
 class NameCheap extends ApiBase
 {
     public const DEFAULT_ATTEMPTS_COUNT = 1, DEFAULT_ATTEMPTS_DELAY = 0.01;
-    public $domains, $ssl, $users, $whoisguard;
+    public $whoisguard;
 
     public function __construct($ApiUser, $ApiKey, $UserName, $ClientIP, $sandbox = false, $attempts_count = self::DEFAULT_ATTEMPTS_COUNT, $attempts_delay = self::DEFAULT_ATTEMPTS_DELAY) {
         parent::__construct($ApiUser, $ApiKey, $UserName, $ClientIP, $sandbox, $attempts_count, $attempts_delay);
@@ -17,9 +17,6 @@ class NameCheap extends ApiBase
 
     private function setupBase(): void
     {
-        $this->domains = new routes\Domains($this);
-        $this->ssl = new routes\Ssl($this);
-        $this->users = new routes\Users($this);
         $this->whoisguard = new routes\Whoisguard($this);
     }
 
